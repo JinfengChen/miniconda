1dbd0df18b5ef8a77150bae934f7e25ae9ffce1b
JinfengChen <jinfeng7chen@gmail.com>
2016-11-09 17:24:37 -0800

    RelocaTE2 recipes

--- work.sh
+++ work.sh
@@ -9,6 +9,8 @@ conda install -c bioconda whatshap
 echo "add bwa to bioconda-recipes"
 git clone https://github.com/JinfengChen/bioconda-recipes.git
 #add recipes in recipes/bwa/0.6.2/
+git checkout -b relocate2
+git branch
 git add recipes/bwa/0.6.2/** 
 git commit -m "bwa_v0.6.2"
 git remote add update https://github.com/JinfengChen/bioconda-recipes.git
@@ -25,7 +27,35 @@ conda config --add channels conda-forge
 conda update conda
 conda install conda-build
 git clone https://github.com/conda/conda-recipes.git
+#install ncurses, which cause problem when using -n
+#conda build conda-recipes/ncurses/
+#cd conda-recipes/ncurses/
+#conda install ncurses --use-local -n relocate2 -m
+#install relocate2
 cd conda-recipes/relocate2
 conda build .
-conda install relocate2 --use-local
+conda install relocate2 --use-local -n relocate2 -m
 #conda search bwa
+#resintall relocate2
+conda remove relocate2 -n relocate2
+conda build .
+conda install relocate2 --use-local -n relocate2 -m
+#conda user install
+#--mkdir Create the environment directory if necessary
+#--use-local Use locally built packages
+#--name Name of environment (in envs dirs).
+conda install --mkdir --use-local --name RelocaTE2 relocate2
+#conda user uninstall
+#remove only relocate2
+conda remove --name RelocaTE2 relocate2
+#remove all env
+conda remove --name RelocaTE2 --all
+
+
+echo "add relocate2 to bioconda-recipes"
+git branch
+git add recipes/relocate2/**
+git commit -m "RelocaTE2.v2.0.0"
+git remote add update https://github.com/JinfengChen/bioconda-recipes.git
+git push --set-upstream origin relocate2
+
